d931c53d71faa1f27ea9846c8093d888e130584b
ojelcodes <ojelngwu@gmail.com>
2022-04-04 18:23:38 +0100

    new update

--- signup.js
+++ signup.js
@@ -2,9 +2,14 @@ var form= document.querySelector('form');
 form.addEventListener('submit',(event)=>{
     event.preventDefault()
     var userPassword=document.getElementById('password').value;
+    var passWord1 = document.getElementById('initial-password').value
     var userEmail= document.getElementById('email').value;
+    if (passWord1!== userPassword) {
+        console.log(alert('password is in correct'))
+    } return user
     var user ={
         email:userEmail,
+        password1: passWord1,
         password:userPassword
     };
     var json=JSON.stringify(user);
