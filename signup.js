e7e3b408326d442081e298a40b6a042b38c37199
favourwright <gismozfexie@yahoo.com>
2022-04-04 18:52:27 +0100

    signup implementation intervention

--- signup.js
+++ signup.js
@@ -1,22 +1,38 @@
 var form= document.querySelector('form');
-form.addEventListener('submit',(event)=>{
-    event.preventDefault()
+form.addEventListener('submit', signUp)
+
+// function that gets form data and signs up the user
+function signUp(event){
+    event.preventDefault();
+    // create default array to store users if it does not exist on local storage
+    if(!localStorage.getItem('users')){
+        localStorage.setItem('users', JSON.stringify([]));
+    }
+
     var userPassword=document.getElementById('password').value;
     var passWord1 = document.getElementById('initial-password').value
     var userEmail= document.getElementById('email').value;
-    if (passWord1!== userPassword) {
-        console.log(alert('password is in correct'))
-    } 
+    var errorField= document.getElementById('error-message');
+
+    if (passWord1 !== userPassword) {
+        console.log(alert('password is in incorrect'))
+        errorField.innerHTML= 'password is incorrect';
+        return
+    }
     var user ={
         email:userEmail,
-        password1: passWord1,
         password:userPassword
     };
-    var json=JSON.stringify(user);
-    localStorage.setItem('user',json)
-    let userDetails = localStorage.getItem('user');
 
-
-        console.log(userDetails)
-})
- 
+    // check if users exist in local storage, if yes push new user to the array else create new array and push new user to it
+    var users= JSON.parse(localStorage.getItem('users'));
+    if(users.length>0){
+        users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
+    } else {
+        users=[user];
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+    // redirect to todo page
+    window.location.href='todo.html';
+}
