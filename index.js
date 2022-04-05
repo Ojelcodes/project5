f69c1ab78ace501e4d0a85eed47785867749f538
favourwright <gismozfexie@yahoo.com>
2022-04-05 01:01:23 +0100

    todo intervention, rough but works

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var form= document.querySelector('form');
-form.addEventListener('submit',(event)=>{
-    event.preventDefault()
+form.addEventListener('submit', login)
+ 
+// login function that gets form data, checks local storage,
+// if email and password match records,
+// if yes redirect to todo page, else display error message
+function login(event){
+    event.preventDefault();
     var userPassword=document.getElementById('password').value;
     var userEmail= document.getElementById('email').value;
-    var user ={
-        email:userEmail,
-        password:userPassword
-    };
-    var json=JSON.stringify(user);
-    localStorage.setItem('user',json)
-    let userDetails = localStorage.getItem('user');
+    var errorField= document.getElementById('error-message');
 
-
-        console.log(userDetails)
-})
- 
+    var users= JSON.parse(localStorage.getItem('users'));
+    if(!users || !users.length>0){
+        errorField.innerHTML= 'user not found';
+        return
+    }
+    var user= users.find(user=>user.email===userEmail && user.password===userPassword);
+    if(user){
+        window.location.href='todo.html';
+    } else {
+        errorField.innerHTML= 'email or password is incorrect';
+    }
+}
--- todo.js
+++ todo.js
@@ -1,29 +1,55 @@
-var bttn = document.getElementById('add-item')
-var button = document.getElementById('remove-item')
-var New = document.getElementById('content')
+// var bttn = document.getElementById('add-item')
+// var button = document.getElementById('remove-item')
+// var New = document.getElementById('content')
 
-bttn.addEventListener( 'click' , function(){
-    var wrapper = document.createElement('div');
-    wrapper.classList.add('todo-set')
-    var checkBox = document.createElement('input');
-    checkBox.classList.add('input1')
-    var todoItem = document.createElement('input');
-    todoItem.classList.add('input-area')
-    todoItem.placeholder ='create a new todo'
-    wrapper.appendChild(checkBox);
-     wrapper.appendChild(todoItem);
-    New.appendChild(wrapper);
-});
-button.addEventListener('click',function(){
-    var wrapper = document.createElement('div');
-    wrapper.classList.add('todo-set')
-    var checkBox = document.createElement('input');
-    checkBox.classList.add('input1')
-    var todoItem = document.createElement('input');
-    todoItem.classList.add('input-area')
-    todoItem.placeholder ='create a new todo'
-    wrapper.appendChild(checkBox);
-     wrapper.appendChild(todoItem);
-    New.appendChild(wrapper);
-    New.removeChild(wrapper)
-})
+// function that gets todo from form and renders them in #todo-list
+const todo_list = document.querySelector("#todo-list")
+document.querySelector('form').addEventListener('submit', addTodo);
+
+function addTodo(event){
+    event.preventDefault();
+    var todo= document.getElementById('todo').value;
+    var li= document.createElement('li');
+
+    li.innerHTML= todo;
+    todo_list.appendChild(li);
+    document.getElementById('todo').value='';
+    removeTodo()
+}
+
+// removeTodo function that detaches the li double-clicked from the list
+function removeTodo(){
+    todo_list.querySelectorAll('li').forEach(function(li){
+        li.addEventListener('dblclick', (ev)=>{
+            ev.target.remove();
+        })
+    });
+}
+
+
+
+// bttn.addEventListener( 'click' , function(){
+//     var wrapper = document.createElement('div');
+//     wrapper.classList.add('todo-set')
+//     var checkBox = document.createElement('input');
+//     checkBox.classList.add('input1')
+//     var todoItem = document.createElement('input');
+//     todoItem.classList.add('input-area')
+//     todoItem.placeholder ='create a new todo'
+//     wrapper.appendChild(checkBox);
+//      wrapper.appendChild(todoItem);
+//     New.appendChild(wrapper);
+// });
+// button.addEventListener('click',function(){
+//     var wrapper = document.createElement('div');
+//     wrapper.classList.add('todo-set')
+//     var checkBox = document.createElement('input');
+//     checkBox.classList.add('input1')
+//     var todoItem = document.createElement('input');
+//     todoItem.classList.add('input-area')
+//     todoItem.placeholder ='create a new todo'
+//     wrapper.appendChild(checkBox);
+//      wrapper.appendChild(todoItem);
+//     New.appendChild(wrapper);
+//     New.removeChild(wrapper)
+// })
